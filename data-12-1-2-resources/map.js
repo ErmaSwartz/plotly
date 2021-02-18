a721b75463277cdc81e483f688aeb9a4cf806489
ErmaSwartz <ermahswartz@gmail.com>
2021-02-18 16:14:14 -0700

    first commit

---
+++ data-12-1-2-resources/map.js
@@ -0,0 +1,64 @@
+// var cities = [
+//     {
+//       "Rank": 1,
+//       "City": "San Antonio ",
+//       "State": "Texas",
+//       "Increase_from_2016": "24208",
+//       "population": "1511946"
+//     },
+//     {
+//       "Rank": 2,
+//       "City": "Phoenix ",
+//       "State": "Arizona",
+//       "Increase_from_2016": "24036",
+//       "population": "1626078"
+//     },
+//     {
+//       "Rank": 3,
+//       "City": "Dallas",
+//       "State": "Texas",
+//       "Increase_from_2016": "18935",
+//       "population": "1341075"
+//     }
+// ];
+
+// var cityNames = cities.map(function(city){
+//     return city.City;
+// });
+// console.log(cityNames);
+
+// Note the following:
+
+// cities is an array of objects. Each object has multiple properties, such as Rank, City, and State.
+// The map() method is used to extract only the City property of each object, i.e., San Antonio, Phoenix, and Dallas. During each iteration, the anonymous function inside map() returns only that property of each object.
+// cityNames is an array of only these city names.
+
+// modify the code to extract population of each city 
+var cities = [
+    {
+      "Rank": 1,
+      "City": "San Antonio ",
+      "State": "Texas",
+      "Increase_from_2016": "24208",
+      "population": "1511946"
+    },
+    {
+      "Rank": 2,
+      "City": "Phoenix ",
+      "State": "Arizona",
+      "Increase_from_2016": "24036",
+      "population": "1626078"
+    },
+    {
+      "Rank": 3,
+      "City": "Dallas",
+      "State": "Texas",
+      "Increase_from_2016": "18935",
+      "population": "1341075"
+    }
+];
+
+var cityPop = cities.map(function(population){
+    return population.Population;
+});
+console.log(cityPop);
